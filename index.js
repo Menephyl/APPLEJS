08ba4339e15822402f4660ef9b2166578623dd1b
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-02-01 18:42:34 -0300

    commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const imagemVisualizacao =  document.getElementById('imagem-visualizacao')
+const nomeCor = document.getElementById('nome-cor-selecionada')
+const tituloProduto = document.getElementById('titulo-produto') 
+const miniaturaImagem0 = document.getElementById('0-imagem-miniatura')
+const miniaturaImagem1 = document.getElementById('1-imagem-miniatura')
+const miniaturaImagem2 = document.getElementById('2-imagem-miniatura')
+
 
 const verdeCipreste ={
     nome:'Verde-cipreste',
@@ -23,11 +29,45 @@ const rosaClaro = {
     pasta: 'imagens-rosa-claro'
 }
 const opcoesCores =[verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro];
+const opcoesTamanho =['41mm','45mm'];
 
 let imagemSelecionada = 1;
-
+let tamanhoSelecionado = 1;
+let corSelecionada = 1;
 function trocarImagem(){
-    const iDopcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id
+    const iDopcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
     imagemSelecionada = iDopcaoSelecionada.charAt(0);
-    imagemVisualizacao.src ='./imagens/opcoes-cores/imagens-azul-inverno/imagem-' +imagemSelecionada+ '.jpeg'
+    imagemVisualizacao.src ='./imagens/opcoes-cores/imagens-azul-inverno/imagem-' +imagemSelecionada+ '.jpeg';
 } // charAt= caractere que está na posição
+
+
+function trocarTamanho(){
+    // autalizar variavel de controle da caixa 
+const iDopcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
+tamanhoSelecionado = iDopcaoSelecionada.charAt(0);
+    //mudar o titulo do produto
+    tituloProduto.innerText = "Pulseira loop esportiva azul-inverno para caixa de  " + opcoesTamanho[tamanhoSelecionado];
+    //mudar o tamanho da imagem de acordo com a opção
+    if(opcoesTamanho[tamanhoSelecionado]==='41mm'){
+    imagemVisualizacao.classList.add('caixa-pequena')
+    } else{
+        imagemVisualizacao.classList.remove('caixa-pequena')
+    }
+}
+function trocarCor(){
+    // atualizar cor selecionada 
+const iDopcaoSelecionada = document.querySelector('[name="opcao-cor"]:checked').id;
+corSelecionada = iDopcaoSelecionada.charAt(0);
+// trocar titulo da página 
+    tituloProduto.innerText = "Pulseira loop esportiva " + opcoesCores[corSelecionada].nome   + " para caixa de  " + opcoesTamanho[tamanhoSelecionado];
+    // trocar nome da cor
+    nomeCor.innerText = "Cor - " +opcoesCores[corSelecionada].nome;
+
+    // trocar imagens das miniaturas 
+    miniaturaImagem0.src = './imagens/opcoes-cores/' + opcoes-cores[corSelecionada].pasta + "/imagem-0.jpg";
+    miniaturaImagem1.src = './imagens/opcoes-cores/' + opcoes-cores[corSelecionada].pasta + "/imagem-1.jpg";
+    miniaturaImagem2.src = './imagens/opcoes-cores/' + opcoes-cores[corSelecionada].pasta + "/imagem-2.jpg";
+    // trocar imagens visulização
+imagemVisualizacao.src = '/imagens/opcoes-cores' + opcoes-cores[corSelecionada].pasta + '/imagem- '+imagemSelecionada + '.jpeg';
+
+} 
